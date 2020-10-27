a47fa14449738125e6aa61470571771a5bbf9269
Lightgoddess <aanya.varma@outlook.com>
2020-10-27 23:07:54 +0000

    Initial commit

---
+++ main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(){
+    int p = 1, q = 2;
+    char *foo = "Aanya";
+
+    printf("The value of p is %d and that of q is %d\n", p, q);
+    printf("The value of s is %s", foo);
+
+    return 0;
+}
